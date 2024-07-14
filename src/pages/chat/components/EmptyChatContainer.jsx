24eed98781e45d14e6c79f154a8306c9f2a5fb48
LVSSandeepKumar <lvssandeepkumar12@gmail.com>
2024-07-14 13:42:58 +0530

    Empty Chat Container Screen

--- src/pages/chat/components/EmptyChatContainer.jsx
+++ src/pages/chat/components/EmptyChatContainer.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { animationDefaultOptions } from "@/lib/utils";
+import Lottie from "react-lottie";
 
 const EmptyChatContainer = () => {
   return (
-    <div>EmptyChatContainer</div>
-  )
-}
+    <div className="flex-1 bg-[#1c1d25] md:flex flex-col justify-center items-center duration-1000 transition-all hidden ">
+      <Lottie 
+        isClickToPauseDisabled={true}
+        height={200}
+        width={200}
+        options={animationDefaultOptions}
+      />
+      <div className="flex flex-col gap-5 text-opacity-80 text-white items-center mt-10 lg:text-4xl text-3xl text-center transition-all duration-300">
+        <h3 className="poppins-medium">
+          Welcome to <span className="text-purple-500">Chat Nexus</span>
+        </h3>
+      </div>
+    </div>
+  );
+};
 
-export default EmptyChatContainer
+export default EmptyChatContainer;
